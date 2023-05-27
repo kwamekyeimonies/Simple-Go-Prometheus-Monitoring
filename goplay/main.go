d108e9e03669a36dc23be84fd1bbe12a0c215c61
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-05-27 13:35:24 +0000

    basic prometheus monitoring

--- goplay/main.go
+++ goplay/main.go
@@ -2,12 +2,33 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "processed_ops_total",
+		Help: "The total number of processed events",
+	})
+)
+
+func RecordMetrics() {
+	go func() {
+		for {
+			opsProcessed.Inc()
+			time.Sleep(time.Second * 2)
+		}
+	}()
+}
+
 func main() {
 
+	RecordMetrics()
+
 	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":7070", nil)
+	http.ListenAndServe(":9090", nil)
 }
