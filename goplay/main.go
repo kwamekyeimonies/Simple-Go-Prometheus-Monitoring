72f7b2cb0d3a84e5f116718fc94d0d2bf12fe8f9
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-05-27 13:28:16 +0000

    basic prometheus setup

--- goplay/main.go
+++ goplay/main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-	router := mux.NewRouter()
 
-
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
-	router.Path("/prometheus").Handler(promhttp.Handler())
-
-	err := http.ListenAndServe(":9090", router)
-	log.Fatal(err)
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":7070", nil)
 }
